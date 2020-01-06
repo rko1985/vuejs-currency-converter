a6676c4161d91ac881ac3d83c952b5025416b2e3
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-06 11:41:42 -1000

    making conversion calculation

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ new Vue({
         currencies: {},
         amount: 0,
         from: 'EUR',
-        to: 'USD'
+        to: 'USD',
+        result: 0
     },
     computed: {
         formattedCurrencies(){
             return Object.values(this.currencies);
+        },
+        calculateResult(){
+            return (Number(this.amount) * this.result).toFixed(2);
         }
     },
     methods: {
@@ -25,6 +29,15 @@ new Vue({
                 this.currencies = response.data.results;
                 localStorage.setItem('currencies', JSON.stringify(response.data.results))
             });
+        },
+        convertCurrency(){
+            const key = `${this.from}_${this.to}`;
+
+            axios.get(`https://free.currconv.com/api/v7/convert?q=${key}&compact=ultra&apiKey=3203da39401131f777d9`)
+                .then((response) => {
+                    console.log(response.data[key]);
+                    this.result = response.data[key];
+                })
         }
     },
     mounted(){
