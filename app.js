97d664c17d52c4f4724353aca91538159fb19420
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-06 10:11:06 -1000

    caching currencies to limit api requests

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ new Vue({
     data: {
         currencies: {}
     },
-    mounted(){
-        axios.get('https://free.currconv.com/api/v7/currencies?apiKey=3203da39401131f777d9')
+    methods: {
+        getCurrencies(){
+            const currencies = localStorage.getItem('currencies')
+
+            if(currencies){
+                this.currencies = JSON.parse(currencies);
+                return;
+            }
+            
+            axios.get('https://free.currconv.com/api/v7/currencies?apiKey=3203da39401131f777d9')
             .then(response => {
                 this.currencies = response.data.results;
-            })
+                localStorage.setItem('currencies', JSON.stringify(response.data.results))
+            });
+        }
+    },
+    mounted(){
+        this.getCurrencies();
     }
 })
